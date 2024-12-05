1ed8c7e8207906cc146df2ca74aa42cb2b330fe5
Divya Meesala <divyameesala22@gmail.com>
2024-12-05 23:58:15 +0530

    Backend

---
+++ STS-workspace/springboot-backend/src/main/java/com/ihub/www/model/Employee.java
@@ -0,0 +1,62 @@
+package com.ihub.www.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+
+
+@Entity
+@Table(name="employees")
+
+
+public class Employee 
+{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column
+	private String firtsName;
+	
+	@Column
+	private String lastName;
+	
+	@Column
+	private String email;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirtsName() {
+		return firtsName;
+	}
+
+	public void setFirtsName(String firtsName) {
+		this.firtsName = firtsName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
